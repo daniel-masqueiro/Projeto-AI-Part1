3991ea314804cae4509b0d227c7fe2dfa98ab974
daniel-masqueiro <dmmos1@iscte-iul.pt>
2026-03-26 13:50:21 +0000

    teste1

--- game/pokemon_fuzzy.py
+++ game/pokemon_fuzzy.py
@@ -10,12 +10,12 @@ e o efeito do ataque e devolve como input a probabilidade de ganhar
 
 level_input = ctrl.Antecedent(np.arange(-10, 11, 1), 'level_input')
 level_input['Muito Abaixo'] = fuzz.trapmf(level_input.universe, [-10, -10, -7, -2])
-level_input['Equilibrado']=fuzz.trimf(level_input.universe, [-5,0,5])
+level_input['Equilibrado'] = fuzz.trimf(level_input.universe, [-5, 0, 5])
 level_input['Muito Alto'] = fuzz.trapmf(level_input.universe, [2, 7, 10, 10])
 
 effect_input = ctrl.Antecedent(np.arange(0, 2.1, 0.1), 'effect_input')
 effect_input['Não Eficaz'] = fuzz.trapmf(effect_input.universe, [0, 0, 0.2, 0.5])
-effect_input['Neutro']=fuzz.trimf(effect_input.universe, [0.5,1,2])
+effect_input['Neutro'] = fuzz.trimf(effect_input.universe, [0.5, 1, 2])
 effect_input['Eficaz'] = fuzz.trapmf(effect_input.universe, [1.5, 2, 2, 2])
 
 win_prob = ctrl.Consequent(np.arange(0, 1.01, 0.01), 'win_prob')
@@ -35,6 +35,7 @@ regra9 = ctrl.Rule(level_input['Muito Alto'] & effect_input['Eficaz'], win_prob[
 
 sistema_combate = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8, regra9])
 simulacao = ctrl.ControlSystemSimulation(sistema_combate)
+
 
 def calculate_prob(level_input, effect_input):
     global_level_input = globals()['level_input']
@@ -63,11 +64,15 @@ def calculate_prob(level_input, effect_input):
     r8_contrib = np.fmin(min(l_alto, e_neutro), win_prob['Alta'].mf)
     r9_contrib = np.fmin(min(l_alto, e_eficaz), win_prob['Alta'].mf)
 
-    else_link = np.fmax(r1_contrib,np.fmax(r2_contrib,np.fmax(r3_contrib,np.fmax(r4_contrib,np.fmax(r5_contrib,np.fmax(r6_contrib,np.fmax(r7_contrib,np.fmax(r8_contrib, r9_contrib))))))))
+    else_link = np.fmax(r1_contrib, np.fmax(r2_contrib, np.fmax(r3_contrib, np.fmax(r4_contrib, np.fmax(r5_contrib,
+                                                                                                        np.fmax(
+                                                                                                            r6_contrib,
+                                                                                                            np.fmax(
+                                                                                                                r7_contrib,
+                                                                                                                np.fmax(
+                                                                                                                    r8_contrib,
+                                                                                                                    r9_contrib))))))))
 
     prob_final = fuzz.defuzz(win_prob.universe, else_link, 'centroid')
 
     return prob_final
-    
-
-
